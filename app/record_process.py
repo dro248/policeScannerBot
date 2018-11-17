489e3c909521afe47f0f1987c7b9e1372c67bfe9
David <david.ostler001@gmail.com>
2018-11-17 07:54:58 -0700

    adds file overwrite, timed polling, and safe turnoff

--- app/record_process.py
+++ app/record_process.py
@@ -11,11 +11,8 @@ class RecorderProcess(Process):
         dest_file = args[1]
         # print(self.__dict__['_args'])
 
-        # print("stream_url: ", stream_url)
-        # print("dest_file: ", dest_file)
-        
         self.x = ffmpeg.input(stream_url).output(dest_file)
-        self.x.run(quiet=True)
+        self.x.run(quiet=True, overwrite_output=True)
 
 class StreamRecorder:
     def __init__(self, page_url: str):
@@ -23,46 +20,53 @@ class StreamRecorder:
         self.page_url = page_url
         self.stream_url = UrlFetcher.fetch_url(self.page_url)
 
+
     def record_to(self, dest_file: str):
         if self.recording_process is not None:
                 # terminate recorder process
                 self.stop()
         
         # Create new process (RecorderProcess)
-        self.recording_process = RecorderProcess(target=None, args=(self.stream_url, dest_file))  # type: Process
-        self.recording_process.start()
-
+        self.recording_process = RecorderProcess(target=None, args=(self.stream_url, dest_file))
+        
         # We want to continue recording the stream, even if the url changes
+        self.recording_process.run()
 
 
     def stop(self):
-        print("STOPPING NOW!")
-        # self.record_thread._stop()
-        self.recording_process.terminate()
-        print("FINISHED STOPPING. =D")
+        print("Turning off Recorder...")
+        try:
+            self.recording_process.terminate()
+        except Exception as e:
+            pass
+        print("Stopped Recording. =D")
     
-    def start(self):
+
+    def _run(self):
         """
-        The start function will begin a Poll-Check-Restart loop
+        The start function will begin a Poll-Check-Restart loop. 
+        This should never be called externally (hence the _ prefix).
         """
+        # poll
         while True:
-            # poll
             fresh_stream_url = UrlFetcher.fetch_url(self.page_url)
 
-            # Check
+            # Check if current stream url is different from the new one
             if self.stream_url != fresh_stream_url:
                 self.stream_url = fresh_stream_url
 
                 curr_time = time.strftime('%M_%S')
+                
                 # Restart
                 self.record_to("out" + curr_time + ".mp3")
 
+                # Wait 30s
+                time.sleep(30)
 
 
-# src_url = UrlFetcher.fetch_url('https://www.broadcastify.com/listen/feed/18656/web')
-s = StreamRecorder('https://www.broadcastify.com/listen/feed/18656/web')
-s.record_to("out1.mp3")  # type: StreamRecorder
-
-
-s.stop()
-# print(help(s.__dict__))
+try:
+    s = StreamRecorder('https://www.broadcastify.com/listen/feed/18656/web')
+    s.record_to("out1.mp3")  # type: StreamRecorder
+except KeyboardInterrupt:
+    print("Caught KeyboardInterrupt signal")
+    s.stop()
